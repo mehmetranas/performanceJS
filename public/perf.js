8c29ed40ff8d97e1ccc318cddfe881829291d81e
mehmet <mhmtranas@gmail.com>
2021-04-18 12:17:16 +0300

    prepared post object

--- public/perf.js
+++ public/perf.js
@@ -1,31 +1,47 @@
-// https://developer.mozilla.org/en-US/docs/Web/API/PerformanceEntry
-function getPerformanceEntries() {
-  var result = [];
-  // Use getEntries() to get a list of all performance entries
-  var entries = performance.getEntries();
-  for (var i = 0; i < entries.length; i++) {
-    result.push(storePerformanceEntry(entries[i]));
-  }
+const url = "http://localhost:8082/";
 
-  return result;
-}
-function storePerformanceEntry(perfEntry) {
-  var result = {};
-  var properties = ["name", "entryType", "startTime", "duration"];
+window.addEventListener("load", async () => {
+  const performanceEntries = performance.getEntries();
+  const navigation = performance.getEntriesByType("navigation")[0];
+  const resources = performance.getEntriesByType("resources");
+  const FCP = performance.getEntriesByName("first-contentful-paint")[0];
+  const TTFB = navigation.responseStart - navigation.requestStart;
+  const domLoad =
+    navigation.domContentLoadedEventEnd - navigation.navigationStart;
+  const windowLoadEvent = navigation.loadEventEnd - navigation.loadEventStart;
+  const staticFiles = navigation.domComplete - navigation.domInteractive;
 
-  for (var i = 0; i < properties.length; i++) {
-    // Check each property
-    var supported = properties[i] in perfEntry;
-    if (supported) {
-      result[properties[i]] = perfEntry[properties[i]];
-    } else {
-      console.log("... " + properties[i] + " is NOT supported");
+  const entries = { FCP, TTFB, domLoad, windowLoadEvent, staticFiles };
+
+  console.log(entries);
+
+  // postEntry({});
+});
+
+const success = () => console.log("Performance Analytics works");
+
+const postEntry = (data = {}) => {
+  // Check for sendBeacon support:
+  if ("sendBeacon" in navigator) {
+    // Beacon the requested
+    if (navigator.sendBeacon(`${url}/api`, JSON.stringify(data))) {
+      return success();
     }
   }
+  postEntryWithFetch(data).then(() => success());
+};
 
-  return result;
+async function postEntryWithFetch(data = {}) {
+  // Default options are marked with *
+  const response = await fetch(`${url}/api`, {
+    method: "POST",
+    mode: "cors",
+    cache: "no-cache",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    redirect: "follow",
+    body: JSON.stringify(data),
+  });
+  return response.json();
 }
-
-window.addEventListener("load", () => {
-  const performanceEntries = getPerformanceEntries();
-});
